d79f09ceb5fa138f301caf4cd9a50e2f56137496
EsauKip <esau.kiprono@student.moringaschool.com>
2023-01-27 11:44:13 +0300

    create tutor model

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tutor(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    models.EmailField(_(""), max_length=254)
+    
+
+    def __str__(self):
+        return 
+
+   
