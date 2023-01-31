5c3516a91479d1194950b9e2ec6284d475af34b5
EsauKip <esau.kiprono@student.moringaschool.com>
2023-01-31 12:02:26 +0300

    create users

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,15 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 
-# Create your models here.
 
-class Tutor(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    models.EmailField(_(""), max_length=254)
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self,name,email,password=None):
+        if not email:
+            raise ValueError("Users must have email Adress")
+        
+        
+        
+class UserAccount(AbstractBaseUser,PermissionsMixin):
+    email= models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     
-
+    
+    objects = UserAccountManager()
+    
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    
+    
+    def get_fullname(self):
+        return self.name
+    
+    def get_shortname(self):
+        return self.name
+    
     def __str__(self):
-        return 
+        return self.email
+    
+ 
+
+    
 
    
--- Telex/settings.py
+++ Telex/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Authentication',
     'rest_framework',
+    'djoser',
     'rest_framework.authtoken',
 ]
 
