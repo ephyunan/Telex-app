facb8f7424c0ab2bc1a6b47998531772cf1f430d
EsauKip <esau.kiprono@student.moringaschool.com>
2023-02-02 13:03:07 +0300

    creayte tutor model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django import forms
 
+from email.policy import default
+
+User = get_user_model()
 # Create your models here.
+class Tutor(models.Model):
+    username = models.CharField(max_length=40)
+    
+    email = models.EmailField()
+    
+    def __str__(self):
+        return str(self.username.username)
